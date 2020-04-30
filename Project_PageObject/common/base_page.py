d86f88ab004ad30c783fdf8cd45402202ccfa52e
liseen <939856019@qq.com>
2020-04-30 23:49:35 +0630

    4.30 最后一次提交

--- Project_PageObject/common/base_page.py
+++ Project_PageObject/common/base_page.py
@@ -1,6 +1,5 @@
 # -*- coding utf-8 -*-
 import os
-import  time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from Project_PageObject.common.config_utils import conf
@@ -13,11 +12,11 @@ driver = webdriver.Firefox(executable_path=driver_path)
 
 class BasePage(object):
     def __init__(self,driver):
-        self.driver=driver
+        self.driver=driver    #
 
 
     '''浏览器操作封装----二次封装'''
-    def open_url(self,url=conf.get_zend_path()):
+    def open_url(self,url):
         self.driver.get(url)
         logutils.info('打开url地址%s'%url)   #打印日志
 
@@ -34,29 +33,34 @@ class BasePage(object):
         logutils.info('获取浏览器网页标题')
         return value
 
-#元素识别、操作
+#元素识别、操作  元素操作封装
 
-    #识别元素信息
-    def find_element_info(self,element_info):
+    #识别元素信息封装    核心   element_info=login_page中的元素的识别信息   可以让所有元素显示等待   补齐元素识别方法，加可以加异常
+    def find_element(self,element_info):
         locator_type_name=element_info['locator_type']   #locator_type  定位方式
         locator_value_info=element_info['locator_value'] #定位的元素
         locator_timeout=element_info['timeout']           #超时时间
-        if locator_type_name =='id':
-            locator_type=By.ID
+        if locator_type_name =='id':    #可以用包含等方式
+            locator_type=By.ID          #把字符串的方式改成真正的方式
         elif locator_type_name=='class':
             locator_type=By.CLASS_NAME
         elif locator_type_name=='xpath':
-            locator_type=By.XPATH
+            locator_type=By.XPATH         #self.driver传入浏览器，等待时间   locator_type：定位方式    locator_value_info：元素信息
         element=WebDriverWait(self.driver,locator_timeout).until(lambda x:x.find_element(locator_type,locator_value_info))
+        logutils.info('[%s]元素识别成功'%element_info['element_name'])   #元素名称
         return element
 
+    #点击操作封装    element_info：元素信息字典
     def click(self,element_info):
         element=self.find_element(element_info)
         element.click()
+        logutils.info('[%s]元素进行点击操作'%element_info['element_name'])
 
+    #输入操作封装   content：输入内容
     def input(self,element_info,content):
         element=self.find_element(element_info)
         element.send_keys(content)
+        logutils.info('[%s]元素输入内容： %s'%(element_info['element_name'],content))
 
-basepage=BasePage(driver)
+
 
--- Project_PageObject/elemet_infos/login_page_01.py
+++ Project_PageObject/elemet_infos/login_page_01.py
@@ -1,21 +1,19 @@
 # --*utf-8*--
 import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from Project_PageObject.common.config_utils import conf
 from Project_PageObject.common.logutils import logutils
 from Project_PageObject.common.base_page import BasePage
-from Project_PageObject.common.base_page import basepage
-
 
 
 class LoginPage(BasePage):
     def __init__(self,driver):
-        super().__init__(driver)
-        self.username_inputbox={'element_name':'用户名输入框',
-                                'locator_type':'xpath',
-                                'locator_value':'//input[@name="account"]',
-                                'timeout':5}
+        super().__init__(driver)    #父类有self.driver的构造，子类也有这个构造，因此要显示调用父类的构造
+        '''元素的识别信息，可以放数据库或者文件中'''
+        self.username_inputbox={'element_name':'用户名输入框',  #元素名字
+                                'locator_type':'xpath',#元素的定位方式
+                                'locator_value':'//input[@name="account"]',  #元素定位的值
+                                'timeout':5}   #超时时间
 
         self.password_inputbox={'element_name':'输入密码',
                                 'locator_type':'xpath',
@@ -26,28 +24,27 @@ class LoginPage(BasePage):
                                 'locator_type':'xpath',
                                 'locator_value':'//button[@id="submit"]',
                                 'timeout':5}
-
+    #代码逻辑：这是一个元素信息（字典），首先调用父类input方法，首先会调用find_element方法查找元素
     def input_username(self,username):
-        basepage.input(self.username_inputbox,username)
-        logutils.info('输入用户名'+str(username))     #输出日志
+        self.input( self.username_inputbox,username )        #输入元素信息字典与对应的元素的内容：content
+        logutils.info('输入用户名'+str(username))     #输出日志,这些位置最好不要加日志，都加在父类
 
     def input_password(self,password):
-        basepage.input(self.password_inputbox,password)
+        self.input(self.password_inputbox,password)
         logutils.info('输入密码'+str(password))
 
     def click_login(self):
-        basepage.click(self.login_button)
+        self.click(self.login_button)
         logutils.info('点击登录按钮')
 
-
-
-if __name__=="__main__":
+if  __name__=="__main__":
+    '''打开浏览器，显示构造driver'''
     current_path = os.path.dirname(__file__)
     driver_path = os.path.join(current_path, conf.get_foxfire_path())
     driver=webdriver.Firefox(executable_path=driver_path)
 
-    loginpage = LoginPage(driver)
-    loginpage.open_url()
-    loginpage.input_username('admin')
-    loginpage.input_password('P1666666,')
+    loginpage = LoginPage(driver)   #实例化driver操作
+    loginpage.open_url('http://106.53.50.202:8999/zentao6/www/user-login.html')  #调用父类中打开浏览器的方法
+    loginpage.input_username(conf.get_username())   #调用字类输入用户名的方法输入用户名
+    loginpage.input_password(conf.get_password())
     loginpage.click_login()
