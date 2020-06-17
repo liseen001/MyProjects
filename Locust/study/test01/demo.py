4e64e56319b7887f1cb7d11cf5ea515ec4492569
Administrator <939856019@qq.com>
2020-06-17 22:07:15 +0800

    6.12 优化基于微信开发者文档接口的接口线性框架

--- Locust/study/test01/demo.py
+++ Locust/study/test01/demo.py
@@ -4,27 +4,11 @@
 # @file: demo.py
 # @time: 2020/5/13 23:09
 # @desc:
-from locust import HttpLocust,TaskSet,task,events
-from gevent._semaphore import  Semaphore
-import queue,pymysql,requests,threading
-
-'''
-创建集合点，当locust实例产生完成时触发
-'''
-
-all_locusts_spawned = Semaphore()
-all_locusts_spawned.acquire()
-
-#创建等待方法
-def on_hatch_complete(**kwargs):
-    all_locusts_spawned.release()
-#当用户加载完时触发
-events.hatch_complete += on_hatch_complete
-
-#全局队列
-q =queue.Queue(maxsize=10)
-
-#token生产函数
-def product():
-    count =1
-    db = pymysql.connect
+coubt = 0
+for i in range(1,5):
+    for j in range(1,5):
+        for k in range(1,5):
+            if (i !=j) and (j!=k) and (i!=k):
+                print(i,j,k)
+                coubt +=1
+print(coubt)
--- beautifulsoup/test/testdemo_01.py
+++ beautifulsoup/test/testdemo_01.py
@@ -4,3 +4,21 @@
 # @file: demo.py
 # @time: 2020/5/13 23:09
 # @desc:
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.imooc.com'
+headers ={'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36'}
+r =requests.get(url=url,headers=headers)
+bs = BeautifulSoup(r.text,'html.parser')
+
+mooc_classess = bs.find_all('h3',class_="course-card-name")  #定位课程信息
+
+class_list = []
+for i in range(len(mooc_classess)):
+    title = mooc_classess[i].text.strip()
+    class_list.append("课程名字：{}\n".format(title))
+
+with open('mooc_classess.txt','a+',encoding='utf-8') as f:
+    for text in class_list:
+        f.write(text)
