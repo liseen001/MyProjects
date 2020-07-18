4ad7cc8611b694a0a973abeca374c3fee0d70fcf
Administrator <939856019@qq.com>
2020-07-18 17:18:42 +0800

    7.14 二次封装requests

--- module_study_samples/others/2、运算符.py
+++ module_study_samples/others/2、运算符.py
@@ -4,7 +4,37 @@
 # @file: demo.py
 # @time: 2020/5/13 23:09
 # @desc:  运算符
-print(5%2)
+import keyword
+import random
 
-
-
+# print(keyword.kwlist)
+# print(keyword.iskeyword)
+# print(r'his a \n girl')
+#
+# set =('20',123,'1323123','asdfasdf')
+# print(set)
+# print(frozenset(set))
+#
+# print(eval('123'))
+# print(type(eval('123')))
+# str2='{"name":"liseen"}'
+# print(type(eval(str2)))
+#
+# print(chr(99))
+# print(ord('c'))
+#
+# # var1 =10
+# # print(var1)
+# # del  var1
+# # print(var1)
+#
+#
+# print(abs(-4))
+# print(max(1,2,3,4))
+# print(pow(2,3))
+# print(round(4.155))
+#
+# print(random.choice(range(10)))
+print(random.randrange(start=2,stop=10,step=2))
+# for i in list[random.randrange(start=2,stop=10,step=2)]:
+#     print(i)
---
+++ module_study_samples/others/3、字符串.py
@@ -0,0 +1,46 @@
+#！/usr/bin/env python
+# encoding: utf-8
+# @author: Mrliu
+# @file: demo.py
+# @time: 2020/5/13 23:09
+# @desc:  字符串
+
+# str1 = 'abcdefghijkoasdaHAVC'
+# print(str1[2:])
+# print('Ki' not in str1)
+#
+# str2 = R'aaal\000'
+# print(str2)
+#
+# print('%s'%('曹尼玛'))
+# print( '我叫%u,今年%d岁'%(2342342,18) )
+
+# name = 'liseen'
+# print(f'hellp {name}')
+# print(type({1+2}))
+#
+# print(f'w{"name"}:w{"liseen"}')
+#
+# str3 = 'liseen'
+# str4 = 'Ayang'
+# print(str3.capitalize())
+# print(str4.lower())
+#
+# print(str3.center(3))
+# print(str3.count('e'))
+
+str1 ='aaAsSSSss'
+print(str1.capitalize())
+print(str1.lower())
+print(str1.islower())
+str1 = '&'
+print(str1.casefold())
+str1 = 'liseen'
+print(str1.center(20,'&'))
+
+print(str1.count('e',0,len(str1)))
+print(str1.encode(encoding='utf-8',errors='strict'))
+
+str1= 'aaa\tbbb'
+print(str1)
+print(str1.expandtabs(8))
