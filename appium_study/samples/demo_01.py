7dab41f509be8bd8dcc8cbd48bd4734f6c1f7328
liseen <939856019@qq.com>
2020-08-28 23:56:55 +0630

    8.20  nb-log

--- appium_study/samples/demo_01.py
+++ appium_study/samples/demo_01.py
@@ -7,6 +7,14 @@
 import selenium
 from appium import webdriver
 des = {
-    'platformName':'Android',
-
+    'platformName':'Android',  #系统
+    'platformVersion': '',  #版本号
+    'deviceName': 'Samsung',      #设备名称：型号  课随便填
+    'udid':'adb device', #设备号
+    'appPackage':'APP包名',
+    'appActivity':'activity名,前面需要加点',
+    'noReset':True,
+    'unicodeKeyboard':True,  #这个和下面的选项一起写，支持中文
+    'resetKetboard': True
 }
+driver = webdriver.Remote('http://0.0.0.0:4723/wd/hub',des)
