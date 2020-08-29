2d619a935eba3bc83d2b7f2086c8f67f6317164c
Administrator <939856019@qq.com>
2020-08-29 21:03:17 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	API_TEST_FRAME/test_reports/P1P2接口自动化测试报告V1.0/P1P2接口自动化测试报告V1.0.html
    #	API_TEST_FRAME/test_reports/P1P2接口自动化测试报告V1.1/P1P2接口自动化测试报告V1.1.html
    #	API_TEST_FRAME/test_reports/P1P2接口自动化测试报告V1.2/P1P2接口自动化测试报告V1.2.html
    #	API_TEST_FRAME/test_reports/P1P2接口自动化测试报告V1.3/P1P2接口自动化测试报告V1.3.html
    #	API_TEST_FRAME/test_reports/P1P2接口自动化测试报告V1.4/P1P2接口自动化测试报告V1.4.html
    #	API_TEST_FRAME/test_reports/P1P2接口自动化测试报告V1.5/P1P2接口自动化测试报告V1.5.html
    #	API_TEST_FRAME/test_reports/P1P2接口自动化测试报告V1.6/P1P2接口自动化测试报告V1.6.html

--- appium_study/samples/demo_01.py
+++ appium_study/samples/demo_01.py
@@ -6,15 +6,17 @@
 # @desc:
 import selenium
 from appium import webdriver
-des = {
+des ={
     'platformName':'Android',  #系统
-    'platformVersion': '',  #版本号
-    'deviceName': 'Samsung',      #设备名称：型号  课随便填
-    'udid':'adb device', #设备号
-    'appPackage':'APP包名',
-    'appActivity':'activity名,前面需要加点',
+    'platformVersion': '9.0',  #版本号
+    'deviceName': 'vivo 1725',      #设备名称：型号  可随便填
+    'udid':'dfa4d640', #设备号
+    'appPackage':'com.youdao.calculator',
+    'appActivity':'com.youdao.calculator.activities.MainActivity',
     'noReset':True,
     'unicodeKeyboard':True,  #这个和下面的选项一起写，支持中文
-    'resetKetboard': True
+    'resetKeyboard': True
 }
-driver = webdriver.Remote('http://0.0.0.0:4723/wd/hub',des)
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',des)
+
+
